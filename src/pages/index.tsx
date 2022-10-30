931a03e4ae31dbb0f2ab08feedcfb4ce56ab090d
ManoelFer <manoelfernandes15@gmail.com>
2022-10-30 08:20:47 -0300

    ✨ add dinamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Head from 'next/head'
 import style from '../styles/styles.module.scss'
 
 export default function Home() {
   return (
-    <div >
-      <h1 className={style.title}>Primeiro Projeto com Next JS</h1>
-    </div>
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas</title>
+      </Head>
+
+      <div >
+        <h1 className={style.title}>Primeiro Projeto com Next JS</h1>
+      </div>
+    </>
+
   )
 }
