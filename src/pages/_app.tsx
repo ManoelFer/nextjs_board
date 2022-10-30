042e1ade940e30e33ae665c2b3b198f95893ccde
ManoelFer <manoelfernandes15@gmail.com>
2022-10-30 08:36:09 -0300

    ✨ add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
 
+import '../styles/global.scss'
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
