a1fd9ce8883bb45338e88014413e4080f0861b93
ManoelFer <manoelfernandes15@gmail.com>
2022-10-30 07:58:29 -0300

    ✨ add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import style from '../styles/styles.module.scss'
+
 export default function Home() {
   return (
     <div >
-      <h1>Primeiro Projeto com Next JS</h1>
+      <h1 className={style.title}>Primeiro Projeto com Next JS</h1>
     </div>
   )
 }
