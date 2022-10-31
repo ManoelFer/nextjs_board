e8453970db4114603140ded99f12862b3992284e
ManoelFer <manoelfernandes15@gmail.com>
2022-10-30 21:16:38 -0300

    ✨ create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import style from 'styles/styles.module.scss'
+import styles from 'styles/styles.module.scss'
 
 export default function Home() {
   return (
@@ -8,9 +8,21 @@ export default function Home() {
         <title>Board - Organizando suas tarefas</title>
       </Head>
 
-      <div >
-        <h1 className={style.title}>Primeiro Projeto com Next JS</h1>
-      </div>
+      <main className={styles.contentContainerStyle}>
+        <img src='/images/board-user.svg' alt='Ferramenta board' />
+
+        <section className={styles.callToActionStyle}>
+          <h1>Uma ferramenta para seu dia a dia! Escreva, planeje e organize-se...</h1>
+
+          <p>
+            <span>100% Gratuita</span> e online.
+          </p>
+        </section>
+
+        <div className={styles.donatersStyle}>
+          <img src="https://sujeitoprogramador.com/steve.png" alt='Usuário 1' />
+        </div>
+      </main>
     </>
 
   )
